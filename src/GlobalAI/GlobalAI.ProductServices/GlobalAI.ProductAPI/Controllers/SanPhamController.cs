667488ec805ec01b1c8df5d44ce7979f2d0a263c
AllMightNTD <dungtiennguyen140602@gmail.com>
2023-04-17 09:10:08 +0700

    fix sever and front end

--- src/GlobalAI/GlobalAI.ProductServices/GlobalAI.ProductAPI/Controllers/SanPhamController.cs
+++ src/GlobalAI/GlobalAI.ProductServices/GlobalAI.ProductAPI/Controllers/SanPhamController.cs
@@ -10,7 +10,7 @@ using System.Net;
 
 namespace GlobalAI.ProductAPI.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [Route("api/product/sanpham")]
     [ApiController]
     public class SanPhamController : BaseController
