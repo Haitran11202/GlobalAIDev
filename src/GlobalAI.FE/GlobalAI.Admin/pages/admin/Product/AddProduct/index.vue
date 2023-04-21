61d681474bf30c34d98ef25c603eea555e86427c
nghiant <nghiant9608@gmail.com>
2023-04-21 19:58:32 +0700

    Thêm format number input

--- src/GlobalAI.FE/GlobalAI.Admin/pages/admin/Product/AddProduct/index.vue
+++ src/GlobalAI.FE/GlobalAI.Admin/pages/admin/Product/AddProduct/index.vue
@@ -34,8 +34,8 @@
           />
         </div>
         <div>
-          <number-input name="giaBan" v-model="giaBan" label="Giá bán" />
-          <!-- <label
+          <!-- <number-input name="giaBan" v-model="giaBan" label="Giá bán" /> -->
+          <label
             for="giaBan"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >Giá bán</label
@@ -46,7 +46,7 @@
             id="giaBan"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
-          /> -->
+          />
         </div>
 
         <div>
