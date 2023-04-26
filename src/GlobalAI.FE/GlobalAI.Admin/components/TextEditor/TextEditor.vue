2d85babda275d3602254471ac167e780db238ebb
nghiant <nghiant9608@gmail.com>
2023-04-27 01:54:13 +0700

    Thêm plugin image resize

--- src/GlobalAI.FE/GlobalAI.Admin/components/TextEditor/TextEditor.vue
+++ src/GlobalAI.FE/GlobalAI.Admin/components/TextEditor/TextEditor.vue
@@ -9,6 +9,9 @@
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 import htmlEditButton from "quill-html-edit-button";
+// import BlotFormatter from 'quill-blot-formatter'
+// import { ImageResize } from 'quill-image-resize-module';
+import ImageResize from 'quill-image-resize-vue';
 
 const { $moduleImageUploader } = useNuxtApp();
 
@@ -20,7 +23,19 @@ const props = defineProps({
 });
 
 const modules = [
-    $moduleImageUploader, { name: 'htmlEditButton', module: htmlEditButton, options: {} }
+    $moduleImageUploader, { name: 'htmlEditButton', module: htmlEditButton, options: {} },
+    // {
+    //     name: 'blotFormatter',
+    //     module: BlotFormatter,
+    //     options: {/* options */ }
+    // }
+    {
+        name: 'imageResize',
+        module: ImageResize,
+        options: {
+            displaySize: true
+        }
+    }
 ]
 
 const emits = defineEmits(["update:modelValue"]);
