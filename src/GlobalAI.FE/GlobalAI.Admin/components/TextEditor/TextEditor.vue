3aa81083be3269e6255afdebd89931bb7794de2b
nghiant <nghiant9608@gmail.com>
2023-04-27 01:08:15 +0700

    Thêm nút edit html

--- src/GlobalAI.FE/GlobalAI.Admin/components/TextEditor/TextEditor.vue
+++ src/GlobalAI.FE/GlobalAI.Admin/components/TextEditor/TextEditor.vue
@@ -1,13 +1,14 @@
 <template>
     <ClientOnly>
-        <QuillEditor toolbar="full" contentType="html" :content="modelValue"
-            :modules="[$moduleImageUploader]" @update:content="$event => onTextChange($event)" />
+        <QuillEditor toolbar="full" contentType="html" :content="modelValue" :modules="modules"
+            @update:content="$event => onTextChange($event)" />
     </ClientOnly>
 </template>
 
 <script setup>
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import htmlEditButton from "quill-html-edit-button";
 
 const { $moduleImageUploader } = useNuxtApp();
 
@@ -17,6 +18,10 @@ const props = defineProps({
         default: "",
     },
 });
+
+const modules = [
+    $moduleImageUploader, { name: 'htmlEditButton', module: htmlEditButton, options: {} }
+]
 
 const emits = defineEmits(["update:modelValue"]);
 const onTextChange = ($event) => {
