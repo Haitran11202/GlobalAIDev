f294347c9affea7f5e67539ab668937144798c43
nghiant <nghiant9608@gmail.com>
2023-05-10 22:41:05 +0700

    Thêm xử lý sp chi tiết

--- src/GlobalAI/GlobalAI.ProductServices/GlobalAI.ProductRepositories/SanPhamChiTietRepository.cs
+++ src/GlobalAI/GlobalAI.ProductServices/GlobalAI.ProductRepositories/SanPhamChiTietRepository.cs
@@ -134,5 +134,91 @@ namespace GlobalAI.ProductRepositories
             }
         }
 
+        /// <summary>
+        /// Cập nhật sản phẩm chi tiết
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="username"></param>
+        public void UpdateSanPhamChiTiet(SanPhamChiTiet entity, string username)
+        {
+            _logger.LogInformation($"{nameof(UpdateSanPhamChiTiet)}: lisentitytIdThuocTinhGiaTri={JsonSerializer.Serialize(entity)}");
+
+            var spChiTiet = _dbSet.FirstOrDefault(x => !x.Deleted && x.Id == entity.Id).ThrowIfNull(_globalAIDbContext, ErrorCode.ProductSpChiTietNotFound);
+
+            checkSpChiTietInUsed(spChiTiet.Id);
+
+            spChiTiet.ModifiedBy = username;
+            spChiTiet.ModifiedDate = DateTime.Now;
+
+            spChiTiet.MaSanPhamChiTiet = entity.MaSanPhamChiTiet;
+            spChiTiet.SoLuong = entity.SoLuong;
+            spChiTiet.MoTa = entity.MoTa;
+            spChiTiet.GiaBan = entity.GiaBan;
+            spChiTiet.GiaChietKhau = entity.GiaChietKhau;
+            spChiTiet.GiaToiThieu = entity.GiaToiThieu;
+            spChiTiet.Thumbnail = entity.Thumbnail;
+        }
+
+        /// <summary>
+        /// Xóa mềm các thuộc tính giá trị của sản phẩm chi tiết
+        /// </summary>
+        /// <param name="listIdThuocTinhGiaTri"></param>
+        /// <param name="username"></param>
+        public void DeleteSanPhamChiTietThuocTinh(List<int> listIdThuocTinhGiaTri, string username)
+        {
+            _logger.LogInformation($"{nameof(DeleteSanPhamChiTietThuocTinh)}: listIdThuocTinhGiaTri={JsonSerializer.Serialize(listIdThuocTinhGiaTri)}; username={username}");
+
+            var now = DateTime.Now;
+            var listThuocTinhGiaTri = _globalAIDbContext.SanPhamChiTietThuocTinhs.Where(x => !x.Deleted && listIdThuocTinhGiaTri.Contains(x.Id));
+            foreach (var item in listThuocTinhGiaTri)
+            {
+                item.Deleted = true;
+                item.DeletedDate = now;
+                item.DeletedBy = username;
+            }
+        }
+
+        /// <summary>
+        /// Get list sản phẩm chi tiết thuộc tính theo id sản phẩm chi tiết
+        /// </summary>
+        /// <param name="idSanPhamChiTiet"></param>
+        /// <returns></returns>
+        public List<SanPhamChiTietThuocTinh> GetListSanPhamChiTietThuocTinh(int idSanPhamChiTiet)
+        {
+            _logger.LogInformation($"{nameof(GetListSanPhamChiTietThuocTinh)}: idSanPhamChiTiet={idSanPhamChiTiet}");
+
+            return _globalAIDbContext.SanPhamChiTietThuocTinhs.Where(x => !x.Deleted && x.IdSanPhamChiTiet == idSanPhamChiTiet).ToList();
+        }
+
+        /// <summary>
+        /// Xoá mềm sản phẩm chi tiết
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="username"></param>
+        public void DeleteSanPhamChiTiet(int id, string username)
+        {
+            _logger.LogInformation($"{nameof(DeleteSanPhamChiTiet)}: id={id}, username={username}");
+
+            var spChiTiet = _dbSet.FirstOrDefault(x => !x.Deleted && x.Id == id).ThrowIfNull(_globalAIDbContext, ErrorCode.ProductSpChiTietNotFound);
+            checkSpChiTietInUsed(id);
+
+            spChiTiet.DeletedBy = username;
+            spChiTiet.DeletedDate = DateTime.Now;
+            spChiTiet.Deleted = true;
+        }
+
+        /// <summary>
+        /// Check sp chi tiết có đang được sử dụng ko
+        /// </summary>
+        /// <param name="id"></param>
+        private void checkSpChiTietInUsed(int id)
+        {
+            var isInChiTietDonHang = _globalAIDbContext.ChiTietDonHangs.Any(x => !x.Deleted && x.IdSanPhamChiTiet == id);
+
+            if (isInChiTietDonHang)
+            {
+                ThrowException(ErrorCode.ProductSpChiTietDaDuocTaoDonHang);
+            }
+        }
     }
 }
