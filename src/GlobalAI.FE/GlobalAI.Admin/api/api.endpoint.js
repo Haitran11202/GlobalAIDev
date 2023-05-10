b4ae6419fc9de621e44d170af098a3e03e56a428
AllMightNTD <dungtiennguyen140602@gmail.com>
2023-05-10 23:12:58 +0700

    Ve giao dien trang Nha Cung Cap , get API san pham trang Home

--- src/GlobalAI.FE/GlobalAI.Admin/api/api.endpoint.js
+++ src/GlobalAI.FE/GlobalAI.Admin/api/api.endpoint.js
@@ -12,6 +12,9 @@ export const API_ENDPOINT = {
   getFullSanPham: (id) =>
     `api/product/sanpham/danh-muc/${id}?pageSize=-1&pageNumber=1&Skip=0`,
   getDanhMucSanPham: `/api/product/sanpham/danh-muc?pageSize=-1&pageNumber=1&Skip=0`,
+  
+    // Get sản phẩm mới nhất
+  getSanPhamTrangHome:(SortBy , SortOrder) => `/api/product/sanpham/home-page?SortBy=${SortBy}&SortOrder%20=${SortOrder}&pageSize=-1&pageNumber=15&Skip=0`,
 
   getSanPhamByIdGStore: (pageSize, pageNumber, skip) =>
     `api/product/sanpham/get-sanpham-gstore?pageSize=${pageSize}&pageNumber=${pageNumber}&skip=${skip}`,
--- src/GlobalAI.FE/GlobalAI.Admin/composables/useApiProduct.js
+++ src/GlobalAI.FE/GlobalAI.Admin/composables/useApiProduct.js
@@ -60,6 +60,15 @@ export const getSanPhamByIdGStore = async (pageSize, pageNumber, skip) => {
     }
 };
 
+export const getSanPhamNews = async (SortBy , SortOrder) => {
+    try {
+        const response = await http.get(API_ENDPOINT.getSanPhamTrangHome(SortBy , SortOrder));
+        return Promise.resolve(response.data);
+    } catch (error) {
+        return Promise.reject(error)
+    }
+}
+
 
 // Cương code
 
