6164871ad1a68636da1d34e5df6b0cb0100f4525
cuongthieu <it.thieutrancuong@gmail.com>
2023-05-10 09:21:59 +0700

    update

---
+++ src/GlobalAI.FE/GlobalAI.Admin/pages/admin/CategoryAttribute/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <card-category-attribute></card-category-attribute>
+</template>
+
+<script>
+import CardCategoryAttribute from "../../../components/Cards/CardCategoryAttribute.vue";
+export default {
+  components: { CardCategoryAttribute },
+};
+</script>
+
+<style></style>
