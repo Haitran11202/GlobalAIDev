966887625cded2928ffc678e2c25ab084fa36245
nghiant <nghiant9608@gmail.com>
2023-05-11 22:38:00 +0700

    Cài lib pagination

--- src/GlobalAI.FE/GlobalAI.Admin/components/Cards/CardProduct.vue
+++ src/GlobalAI.FE/GlobalAI.Admin/components/Cards/CardProduct.vue
@@ -3,16 +3,11 @@
     <div class="relative w-full overflow-x-auto">
       <div class="mb-0 rounded-md px-4 py-3 bg-[#fff] border-0">
         <div class="flex flex-wrap items-center">
-          <div
-            class="relative w-full px-4 max-w-full flex justify-between items-center"
-          >
+          <div class="relative w-full px-4 max-w-full flex justify-between items-center">
             <h3 class="font-semibold text-lg text-slate-800 uppercase">
               Danh sách sản phẩm
             </h3>
-            <button
-              @click="$router.push('/admin/product/addproduct')"
-              class="btn btn-outline"
-            >
+            <button @click="$router.push('/admin/product/addproduct')" class="btn btn-outline">
               Thêm sản phẩm
             </button>
           </div>
@@ -40,11 +35,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr
-            class="text-sm hover:bg-slate-400"
-            v-for="product in products"
-            :key="product.id"
-          >
+          <tr class="text-sm hover:bg-slate-400" v-for="product in products" :key="product.id">
             <th>
               <label>
                 <input type="checkbox" class="checkbox" />
@@ -55,18 +46,15 @@
               <div class="flex items-center space-x-3">
                 <div class="avatar">
                   <div class="mask mask-squircle w-12 h-12">
-                    <img
-                      :src="getImageUrl(product.thumbnail)"
-                      alt="Avatar Tailwind CSS Component"
-                    />
+                    <img :src="getImageUrl(product.thumbnail)" alt="Avatar Tailwind CSS Component" />
                   </div>
                 </div>
                 <div>
                   <div class="font-bold">
                     {{
                       product.tenSanPham.length > 30
-                        ? product.tenSanPham.slice(0, 30) + "..."
-                        : product.tenSanPham
+                      ? product.tenSanPham.slice(0, 30) + "..."
+                      : product.tenSanPham
                     }}
                   </div>
                 </div>
@@ -93,8 +81,8 @@
             <td>
               {{
                 product.ngayDangKi
-                  ? $moment(product.ngayDangKi).format("DD/MM/YYYY")
-                  : ""
+                ? $moment(product.ngayDangKi).format("DD/MM/YYYY")
+                : ""
               }}
             </td>
             <!-- <td class="whitespace-normal">
@@ -121,18 +109,9 @@
             <td>Đã duyệt</td>
             <td>
               <div class="dropdown dropdown-left dropdown-end">
-                <label
-                  tabindex="0"
-                  class="btn m-1 btn-outline"
-                  @click="toggleDropdown"
-                  >...</label
-                >
-                <ul
-                  tabindex="0"
-                  class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-                  v-if="isOpen"
-                  @click="closeDropdown"
-                >
+                <label tabindex="0" class="btn m-1 btn-outline" @click="toggleDropdown">...</label>
+                <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52" v-if="isOpen"
+                  @click="closeDropdown">
                   <li @click="onEditButtonClick(product.id)"><a>Sửa</a></li>
                   <li @click="onDeleteButtonClick(product.id)"><a>Xoá</a></li>
                   <li><a>Duyệt</a></li>
@@ -144,58 +123,14 @@
       </table>
     </div>
     <div class="btn-group flex justify-center mt-2">
-      <button @click="previousPage" class="btn">«</button>
-      <button class="btn">Trang {{ pageNumber }}</button>
-      <button @click="nextPage" class="btn">»</button>
+      <Pagination v-model="pageNumber" @page-change="fetchData" :page-count="pageTotalItems / pageSize" />
     </div>
   </div>
 </template>
-<script>
-import TableDropdown from "../Dropdowns/TableDropdown.vue";
-
-import bootstrap from "../../assets/img/bootstrap.jpg";
-import angular from "../../assets/img/angular.jpg";
-import sketch from "../../assets/img/sketch.jpg";
-import react from "../../assets/img/react.jpg";
-import vue from "../../assets/img/react.jpg";
-
-import team1 from "../../assets/img/team-1-800x800.jpg";
-import team2 from "../../assets/img/team-2-800x800.jpg";
-import team3 from "../../assets/img/team-3-800x800.jpg";
-import team4 from "../../assets/img/team-4-470x470.png";
-import { toast } from "vue3-toastify";
-
-export default {
-  data() {
-    return {
-      bootstrap,
-      angular,
-      sketch,
-      react,
-      vue,
-      team1,
-      team2,
-      team3,
-      team4,
-    };
-  },
-  components: {
-    TableDropdown,
-  },
-
-  props: {
-    color: {
-      default: "light",
-      validator: function (value) {
-        // The value must match one of these strings
-        return ["light", "dark"].indexOf(value) !== -1;
-      },
-    },
-  },
-};
-</script>
 
 <script setup>
+import TableDropdown from "../Dropdowns/TableDropdown.vue";
+import Pagination from '../Pagination/Pagination.vue';
 import { ref, watchEffect } from "vue";
 import { useRouter } from "vue-router";
 
@@ -206,7 +141,8 @@ const config = useRuntimeConfig();
 const baseUrl = config.public.apiEndpoint;
 
 // Khởi tạo giá trị mặc định phân trang 5 1 0
-const pageSize = 15;
+const pageSize = 2;
+const pageTotalItems = ref(1);
 const pageNumber = ref(1);
 const skip = ref(0);
 
@@ -217,17 +153,19 @@ const showAction = ref({});
 const showMore = ref({});
 
 // Lấy tất cả sản phẩm
-const fetchData = async () => {
+const fetchData = () => {
   getSanPhamByIdGStore(pageSize, pageNumber.value, skip.value)
     .then((response) => {
       // Gán giá trị mới vào biến reactive
       products.value = response.data.items;
+      pageTotalItems.value = response.data.totalItems;
       console.log(products.value);
     })
     .catch((err) => {
       console.error(err);
     });
 };
+
 // Hàm này sẽ lấy đường dẫn của ảnh từ server và bind vào thuộc tính src của thẻ img
 const getImageUrl = (imageUrl) => {
   if (!imageUrl) {
@@ -239,22 +177,6 @@ const getImageUrl = (imageUrl) => {
   return url;
 };
 
-const previousPage = () => {
-  if (pageNumber.value === 1) {
-    // Kiểm tra xem đã đạt trang đầu tiên hay chưa
-    return;
-  }
-  pageNumber.value -= 1;
-};
-
-const nextPage = () => {
-  console.log(1);
-  if (products.value.length < pageSize) {
-    // Kiểm tra xem có đủ sản phẩm để hiển thị trên trang tiếp theo hay không
-    return;
-  }
-  pageNumber.value += 1;
-};
 // Gọi hàm xóa sản phẩm khi người dùng click vào nút Xóa
 const onDeleteButtonClick = (id) => {
   deleteProduct(id)
@@ -286,7 +208,7 @@ watchEffect(() => {
   fetchData();
   if (deletedProduct.value !== null) {
     // Nếu sản phẩm đã được xóa thành công, gọi lại hàm getAllProducts() để cập nhật danh sách sản phẩm
-    getAllProducts();
+    // getAllProducts();
     // Đặt lại giá trị cho biến deletedProduct
     deletedProduct.value = null;
   }
